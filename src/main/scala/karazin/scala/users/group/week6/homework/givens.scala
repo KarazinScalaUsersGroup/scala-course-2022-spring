223ede6c34a3ad1dfd52647cba6d81e49d87af7b
dmitriy.kolmogorov <dmitriy.kolmogorov@requestun.com>
2022-06-27 16:16:23 +0300

    week6 task

--- src/main/scala/karazin/scala/users/group/week6/homework/givens.scala
+++ src/main/scala/karazin/scala/users/group/week6/homework/givens.scala
@@ -6,19 +6,37 @@ object givens:
     The trait is used for converting instances to a json string representation
     Provide a type parameter(s) for the trait and the method 
     and argument(s) and a result type
-  */ 
-  
-  trait DummyType
-  
-  trait JsonStringEncoder:
-    def encode: DummyType
+  */
 
-  /* 
-    Enrich the previous solution and
-    * make Map[String, V] convertable to json
-    * make sure that all previous converions and a new one
-      could be done by calling methos `toJsonString` directly 
-      on the target object
-      
-    Add tests for all cases  
-  */ 
+  trait JsonStringEncoder[T]:
+    def encode(elem: T): String
+
+    extension(t: T)
+      def toJsonString(): String =
+        encode(t)
+
+  given IntEncoder: JsonStringEncoder[Int] with
+    (obj: Int): String => obj.toString
+
+  given BoolEncoder: JsonStringEncoder[Boolean] with
+    (obj: Boolean): String => obj.toString
+
+  given StringEncoder: JsonStringEncoder[String] with
+    (obj: String): String => "\"" + src + "\""
+
+  given ListEncoder[T](using encoder: => JsonStringEncoder[T]): JsonStringEncoder[List[T]] with
+    (src: List[T]): String => src.map {
+      _ => _.toJsonString()
+    }.mkString("[", ",", "]")
+
+  given MapEncoder[V](using encoderValue: => JsonStringEncoder[V])(
+    using encoderKey: => JsonStringEncoder[String]): JsonStringEncoder[Map[String, V]] with
+      def encode(src: Map[String, V]): String =
+        src.foldLeft(List[String]()) {
+          case (acc, (key, value)) => acc :+ (key.toJsonString() + ": " + value.toJsonString())
+        }.mkString("{", ",", "}"
+  )
+
+  object JsonStringEncoder:
+    def apply[V](using encoder: => JsonStringEncoder[V]): JsonStringEncoder[V] =
+      encoder
