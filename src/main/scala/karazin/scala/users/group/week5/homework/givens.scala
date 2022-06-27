85157d60e15ade8f5c08c9f634fdd39ad3192f2a
dmitriy.kolmogorov <dmitriy.kolmogorov@requestun.com>
2022-06-27 16:11:09 +0300

    week5 task

--- src/main/scala/karazin/scala/users/group/week5/homework/givens.scala
+++ src/main/scala/karazin/scala/users/group/week5/homework/givens.scala
@@ -6,14 +6,23 @@ object givens:
     The trait is used for converting instances to a json string representation
     Provide a type parameter(s) for the trait and the method 
     and argument(s) and a result type
-  */ 
-  
-  trait DummyType
-  
-  trait JsonStringEncoder:
-    def encode: DummyType
+  */
 
-  /* 
-    Make sure that integers, booleans, strings and lists 
-    are convertable to a json string representation 
-  */ 
+  trait JsonStringEncoder[T]:
+    def encode(src: T): String
+
+  given IntEncoder: JsonStringEncoder[Int] with
+    (int_value: Int): String => int_value.toString
+
+  given BoolEncoder: JsonStringEncoder[Boolean] with
+    (bool_val: Boolean): String => {bool_val.toString}
+
+  given StringEncoder: JsonStringEncoder[String] with
+    (string_obj: String): String => {"\"" + string_obj + "\""}
+
+  given ListEncoder[T](using encoder: => JsonStringEncoder[T]): JsonStringEncoder[List[T]] with
+    def encode(src: List[T]): String = "[" + src.foldLeft(List[String]()) {(acc, src) => acc :+ encoder.encode(src)}.mkString(", ") + "]"
+
+  object JsonStringEncoder:
+    def apply[V](using encoder: => JsonStringEncoder[V]): JsonStringEncoder[V] =
+      encoder
--- src/test/scala/karazin/scala/users/group/week5/homework/GivensSuite.scala
+++ src/test/scala/karazin/scala/users/group/week5/homework/GivensSuite.scala
@@ -2,27 +2,111 @@ package karazin.scala.users.group.week5.homework
 
 import scala.concurrent.Future
 
+import karazin.scala.users.group.week5.homework.givens.{given, _}
+
 /*
   Write test for all programs in karazin.scala.users.group.week4.homework.givens
-
   Make sure that the following cases are tested:
     • json string representation for integers works
     • json string representation for booleans works
     • json string representation for strings works
     • json string representation for lists for integers, booleans and strings works
     • json string representation for maps fails on compile time
-
   Review:
     • https://www.json.org/json-en.html
     • https://scalameta.org/munit/docs/tests.html
     • https://scalameta.org/munit/docs/assertions.html
     • https://scalameta.org/munit/docs/assertions.html#compileerrors
-    
+
   NB: Do not use sync, this homework does not belong async stuff
-    
+
  */
 class GivensSuite extends munit.FunSuite:
-  
-  test("successful test example") {
-    assertEquals(42, 42)
+
+  test("Int even") {
+    assertEquals(
+      JsonStringEncoder[Int]
+        .encode(200), "200"
+    )
+  }
+
+  test("Int odd") {
+    assertEquals(
+      JsonStringEncoder[Int]
+        .encode(117), "117"
+    )
+  }
+
+  test("Int zero") {
+    assertEquals(
+      JsonStringEncoder[Int]
+        .encode(0), "0"
+    )
+  }
+
+  test("Int negative") {
+    assertEquals(
+      JsonStringEncoder[Int]
+        .encode(-7), "-7"
+    )
+  }
+
+  test("Bool false") {
+    assertEquals(
+      JsonStringEncoder[Boolean]
+        .encode(false), "false"
+    )
+  }
+
+  test("Bool JSON true") {
+    assertEquals(
+      JsonStringEncoder[Boolean]
+        .encode(true), "true"
+    )
+  }
+
+  test("String") {
+    assertEquals(
+      JsonStringEncoder[String]
+        .encode("Lorem ipsum dolor"), "\"Lorem ipsum dolor\""
+    )
+  }
+
+  test("String Empty") {
+    assertEquals(
+      JsonStringEncoder[String]
+        .encode(""), "\"\""
+    )
+  }
+
+  test("List String") {
+    assertEquals(
+      JsonStringEncoder[List[String]]
+        .encode("Lorem" :: "ipsum" :: "dolor" :: Nil), "[\"Lorem\", \"ipsum\", \"dolor\"]"
+    )
+  }
+
+  test("List Booleans") {
+    assertEquals(
+      JsonStringEncoder[List[Boolean]]
+        .encode(true :: false :: true :: Nil), "[true, false, true]"
+    )
+  }
+
+  test("List Integers") {
+    assertEquals(
+      JsonStringEncoder[List[Int]]
+        .encode(90 :: -119 :: 0 :: Nil), "[90, -119, 0]"
+    )
+  }
+
+  test("List Empty") {
+    assertEquals(
+      JsonStringEncoder[List[Int]]
+        .encode(Nil), "[]"
+    )
+  }
+
+  test("Map fails") {
+    compileErrors("JsonStringEncoder[Map[String, Int]].encode(Map(\"x\" -> 100, \"y\" -> -2, \"z\" -> 0))")
   }
