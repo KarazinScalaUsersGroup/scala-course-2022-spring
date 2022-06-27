787a5fbf767f7d00e9251e8cac1ff850bfe39b7d
dmitriy.kolmogorov <dmitriy.kolmogorov@requestun.com>
2022-06-27 15:51:43 +0300

    week3 task

--- src/main/scala/karazin/scala/users/group/week3/homework/services.scala
+++ src/main/scala/karazin/scala/users/group/week3/homework/services.scala
@@ -1,17 +1,14 @@
 /*
-
 Fix the code to make it compilable
 Write tests
+*/
 
 package karazin.scala.users.group.week3.homework
-
 import java.util.UUID
 import scala.concurrent.Future
 import scala.util.Success
 import scala.util.Failure
-
 import karazin.scala.users.group.week3.homework.model._
-
 
 object services:
   
@@ -19,33 +16,35 @@ object services:
     Future {
       UserProfile(UUID.randomUUID())
     }
-  
-  // Fix the code make it compilable
+
   def getPosts(userId: UUID): Future[List[Post]] =
     Future {
-      Post(userId = UUID.randomUUID(), postId = UUID.randomUUID()) :: Nil
+      Post(
+        userId = usedId,
+        postId = UUID.randomUUID()
+      ) :: Nil
     }
-  
-  // Fix the code make it compilable
+
   def getComments(postId: UUID): Future[List[Comment]] = 
     Future {
       // Emulating time consumed operation
       Thread.sleep(5000)
-      Comment(userId = UUID.randomUUID(), postId = UUID.randomUUID()) :: Nil
+      Comment(userId = UUID.randomUUID(), postId = postId) :: Nil
     }
   
-  def getLikes(postId: UUID): Future[List[Like]] = 
+  def getLikes(postId: UUID): Future[List[Like] =
     Future {
       // Emulating time consumed operation
       Thread.sleep(2000)
-      Like(userId = UUID.randomUUID(), postId = UUID.randomUUID()) :: Nil
+      Like(userId = UUID.randomUUID(), postId = postId) :: Nil
     }
   
   def getShares(postId: UUID): Future[List[Share]] = 
     Future {
       // Emulating time consumed operation
       Thread.sleep(500)
-      Share(userId = UUID.randomUUID(), postId = UUID.randomUUID()) :: Nil
+      Share(
+        userId = UUID.randomUUID(),
+        postId = postId
+      ) :: Nil
     }
-    
- */
